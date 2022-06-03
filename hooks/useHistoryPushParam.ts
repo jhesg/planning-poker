d0eee6ebedd1b04e6454587926a34824bea1d944
Jhonatan Gonzalez <jhesgodi@gmail.com>
2022-06-03 15:31:42 +1000

    refactor(reset): reset all project

--- hooks/useHistoryPushParam.ts
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-import { historyPushParam } from "@/utils";
-
-export const useHistoryPushParam = () => {
-  const router = useRouter();
-  return (params: Record<string, string>) => historyPushParam(router, params);
-};
--- utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./history";
-export * from "./users";
-export * from "./messages";
